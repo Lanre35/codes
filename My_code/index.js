58af8cbebea4572aa0783bdb433c7e9c85333bcb
Kareem Azeez Olanrewaju <111020047+Lanre35@users.noreply.github.com>
2022-12-25 14:22:25 +0000

    done

--- My_code/index.js
+++ My_code/index.js
@@ -2,3 +2,11 @@ let para = document.querySelector('p')
 para.innerHTML = 'Welcome to my coding git'
 para.style.color = 'green'
 para.style.textAlign = 'center'
+
+const d = document.querySelector('#div')
+d.style.border = '1px solid black'
+
+const ds = document.querySelectorAll('.div')
+ds.forEach(element =>{
+    element.textContent = 'green'
+})
